8a9e5a73d09847d3829c9af70e25662ad93540ab
rizzo08-coder <m.rizzo009@studenti.unibs.it>
2022-08-01 22:19:27 +0200

    aggiunta umiditá, aggiunta classe SwingChar.java per migliorare struttura

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,9 @@
 import javax.swing.*;
 
 public class Main {
-
     public static void main(String[] args) {
         SwingChar swingChar = new SwingChar();
-
         swingChar.drawGraph(swingChar.getDataSetTemperature(), "Temperature Chart", "Temperature", "temperature");
         swingChar.drawGraph(swingChar.getDataSetHumidity(), "Humidity Chart", "Humidity", "humidity");
-
     }
 }
